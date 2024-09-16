a55feb9e51e351435ebd9b5f7cc646448e4d577d
apollodev66 <contact.suriya.jbch@gmail.com>
2024-09-16 14:45:46 +0700

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://192.168.101.2:92/api/login', {
+      const response = await fetch('https://192.168.101.2:92/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
